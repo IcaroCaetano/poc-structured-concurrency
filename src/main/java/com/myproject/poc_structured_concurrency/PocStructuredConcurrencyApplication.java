ba3590cd3ace5202c31818449a536fe459b71795
ICAROCAETANO <icaro.caetano@gmail.com>
2026-05-21 22:33:59 -0300

    first commit

---
+++ src/main/java/com/myproject/poc_structured_concurrency/PocStructuredConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.poc_structured_concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocStructuredConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocStructuredConcurrencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/poc_structured_concurrency/PocStructuredConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.poc_structured_concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocStructuredConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
