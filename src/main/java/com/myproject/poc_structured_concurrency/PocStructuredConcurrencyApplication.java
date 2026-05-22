bab528c9c55ffdbb0d7c2fd7be8b9258d4a321ed
ICAROCAETANO <icaro.caetano@gmail.com>
2026-05-21 23:10:42 -0300

    feat: add service

--- src/main/java/com/myproject/poc_structured_concurrency/PocStructuredConcurrencyApplication.java
+++ src/main/java/com/myproject/poc_structured_concurrency/PocStructuredConcurrencyApplication.java
@@ -1,13 +1,29 @@
 package com.myproject.poc_structured_concurrency;
 
+import com.myproject.poc_structured_concurrency.context.RequestContext;
+import com.myproject.poc_structured_concurrency.service.FraudAnalysisService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class PocStructuredConcurrencyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PocStructuredConcurrencyApplication.class, args);
+
+		var requestId = UUID.randomUUID().toString();
+
+		ScopedValue.where(RequestContext.REQUEST_ID, requestId)
+				.run(() -> {
+
+					var fraudAnalysisService = new FraudAnalysisService();
+
+					var response = fraudAnalysisService.analyze("12345678900");
+
+					System.out.println("\nFINAL RESPONSE");
+					System.out.println(response);
+				});
 	}
 
 }
---
+++ src/main/java/com/myproject/poc_structured_concurrency/service/FraudAnalysisService.java
@@ -0,0 +1,55 @@
+package com.myproject.poc_structured_concurrency.service;// service/FraudAnalysisService.java
+
+import com.myproject.poc_structured_concurrency.dto.FraudAnalysisResponse;
+import com.myproject.poc_structured_concurrency.util.LoggerUtil;
+
+import java.util.concurrent.StructuredTaskScope;
+
+
+public class FraudAnalysisService {
+
+    private final FaceMatchService faceMatchService =
+            new FaceMatchService();
+
+    private final LivenessService livenessService =
+            new LivenessService();
+
+    private final BureauService bureauService =
+            new BureauService();
+
+    public FraudAnalysisResponse analyze(String cpf) {
+
+        LoggerUtil.log("Starting Fraud Analysis");
+
+        try (var scope =
+                     new StructuredTaskScope.ShutdownOnFailure()) {
+
+            var faceTask =
+                    scope.fork(() -> faceMatchService.analyze(cpf));
+
+            var livenessTask =
+                    scope.fork(() -> livenessService.analyze(cpf));
+
+            var bureauTask =
+                    scope.fork(() -> bureauService.analyze(cpf));
+
+            scope.join();
+
+            scope.throwIfFailed();
+
+            LoggerUtil.log("All services finished successfully");
+
+            return new FraudAnalysisResponse(
+                    faceTask.get(),
+                    livenessTask.get(),
+                    bureauTask.get()
+            );
+
+        } catch (Exception e) {
+
+            LoggerUtil.log("Fraud Analysis FAILED");
+
+            throw new RuntimeException(e);
+        }
+    }
+}
